7634a630b09f68e0afde1b50b27fac5addbb1dfb
CoolMan22 <iuploadgmod@gmail.com>
2024-08-23 16:52:01 -0500

    I mean i could just solve that issue by testing locally but..

--- src/functions/tools/fastLog.js
+++ src/functions/tools/fastLog.js
@@ -3,13 +3,13 @@ const chalk = require("chalk");
 module.exports = (client) => {
   client.fastLog = async (messageText, color, start) => {
     const time = Date.now() - start;
-    const color = "#b3b3b3";
+    const bColor = "#b3b3b3";
 
-    let first = await client.color(color, "[");
+    let first = await client.color(bColor, "[");
     let message = chalk.green(messageText);
-    let second = await client.color(color, "] ");
+    let second = await client.color(bColor, "] ");
     let name = await client.color(color, name);
-    let third = await client.color(color, " loaded in");
+    let third = await client.color(bColor, " loaded in");
     let fourth = chalk.yellow(`${time}ms`);
 
     const logMessage = first + message + second + third + fourth;
