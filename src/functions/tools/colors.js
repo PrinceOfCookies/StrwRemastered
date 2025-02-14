e87cc032bcabddb64b7023580e75c4684f34bb13
PrinceOfCookies <prince@strw.club>
2025-02-14 16:54:24 -0600

    Guess it didnt like me doin this

--- src/functions/tools/colors.js
+++ src/functions/tools/colors.js
@@ -1,8 +1,8 @@
-const { hex } = require("chalk");
+const chalk = require("chalk");
 
 module.exports = (client) => {
   client.color = async (hexC, text) => {
-    let hexColor = hex(hexC);
+    let hexColor = chalk.hex(hexC);
     let colorText = hexColor(text);
 
     return colorText;
