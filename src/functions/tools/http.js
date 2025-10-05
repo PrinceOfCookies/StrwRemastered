a8c14193934a28e26a7daf32d6a4924b582dcc4d
PrinceOfCookies <prince@strw.club>
2025-10-05 02:15:35 -0500

    Uninstall axios

--- src/functions/tools/http.js
+++ src/functions/tools/http.js
@@ -1,49 +1,47 @@
-const { get, post } = require("axios");
-
 module.exports = async (client) => {
   client.http = async (url, method, headers, body) => {
-    switch (method) {
-      case "GET":
-        try {
-          let response = await get(url, { headers: headers });
+    // switch (method) {
+    //   case "GET":
+    //     try {
+    //       let response = await get(url, { headers: headers });
 
-          if (response.status < 200 || response.status >= 300) {
-            console.error(
-              "Failed to fetch data. Code:",
-              response.status,
-              "Reason:",
-              response.statusText
-            );
-            return;
-          }
+    //       if (response.status < 200 || response.status >= 300) {
+    //         console.error(
+    //           "Failed to fetch data. Code:",
+    //           response.status,
+    //           "Reason:",
+    //           response.statusText
+    //         );
+    //         return;
+    //       }
 
-          return response.data;
-        } catch (error) {
-          console.error("Error during GET request:", error);
-        }
-        break;
-      case "POST":
-        try {
-          let response = await post(url, body, { headers: headers });
+    //       return response.data;
+    //     } catch (error) {
+    //       console.error("Error during GET request:", error);
+    //     }
+    //     break;
+    //   case "POST":
+    //     try {
+    //       let response = await post(url, body, { headers: headers });
 
-          if (response.status < 200 || response.status >= 300) {
-            console.error(
-              "Failed to post data. Code:",
-              response.status,
-              "Reason:",
-              response.statusText
-            );
-            return;
-          }
+    //       if (response.status < 200 || response.status >= 300) {
+    //         console.error(
+    //           "Failed to post data. Code:",
+    //           response.status,
+    //           "Reason:",
+    //           response.statusText
+    //         );
+    //         return;
+    //       }
 
-          return response.data;
-        } catch (error) {
-          console.error("Error during POST request:", error);
-        }
-        break;
-      default:
-        console.error("Invalid method");
-        break;
-    }
+    //       return response.data;
+    //     } catch (error) {
+    //       console.error("Error during POST request:", error);
+    //     }
+    //     break;
+    //   default:
+    //     console.error("Invalid method");
+    //     break;
+    // }
   };
 };
