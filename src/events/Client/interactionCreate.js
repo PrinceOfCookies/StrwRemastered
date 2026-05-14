90e132525d3b9ccba822df681742594eb082d896
PrinceOfCookies <iuploadgmod@gmail.com>
2026-05-14 10:27:13 -0500

    No buttons rn so dont need this

--- src/events/Client/interactionCreate.js
+++ src/events/Client/interactionCreate.js
@@ -1,9 +1,5 @@
 const {
   MessageFlags,
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
 } = require("discord.js");
 
 module.exports = {
@@ -89,98 +85,6 @@ module.exports = {
           content: "Something went wrong while executing this command!",
           flags: MessageFlags.Ephemeral,
         });
-      }
-    } else if (interaction.isButton()) {
-      const { buttons } = client;
-      const { customId } = interaction;
-      const button = buttons.get(customId);
-      if (!button) return;
-
-      async function handleVote(video, userId, voteType) {
-        const opposite = voteType === "yesVote" ? "noVote" : "yesVote";
-        if (video[voteType].includes(userId)) return false; // already voted
-        if (video[opposite].includes(userId)) {
-          video[opposite] = video[opposite].filter((id) => id !== userId);
-        }
-        video[voteType].push(userId);
-        await video.updateOne({ yesVote: video.yesVote, noVote: video.noVote });
-        return true;
-      }
-
-      function createVoteButtons(disabled = false) {
-        return new ActionRowBuilder().addComponents(
-          new ButtonBuilder()
-            .setCustomId("yes")
-            .setLabel("Good")
-            .setEmoji("👍")
-            .setStyle(ButtonStyle.Success)
-            .setDisabled(disabled),
-          new ButtonBuilder()
-            .setCustomId("no")
-            .setLabel("Bad")
-            .setEmoji("👎")
-            .setStyle(ButtonStyle.Danger)
-            .setDisabled(disabled),
-          new ButtonBuilder()
-            .setCustomId("feedback")
-            .setLabel("Give Feedback")
-            .setEmoji("📝")
-            .setStyle(ButtonStyle.Primary)
-            .setDisabled(disabled)
-        );
-      }
-
-      switch (customId) {
-        case "yes":
-          if (!(await handleVote(video, interaction.user.id, "yesVote"))) {
-            return await interaction.reply({ content: "You already voted!" });
-          }
-          return await interaction.reply({ content: "You voted good!" });
-
-        case "no":
-          if (!(await handleVote(video, interaction.user.id, "noVote"))) {
-            return await interaction.reply({ content: "You already voted!" });
-          }
-          return await interaction.reply({ content: "You voted bad!" });
-
-        case "feedback":
-          return await interaction.reply({
-            content: "Button currently doesn't work",
-          });
-
-        case "nopost":
-          return await button.update({
-            content: "Video not posted",
-            components: [createVoteButtons(true)],
-          });
-
-        case "post":
-          const embed = new EmbedBuilder()
-            .setColor(0x5fb041)
-            .setTitle(video.title)
-            .setDescription(
-              "What did you think of it? Click the buttons below to vote"
-            )
-            .setThumbnail(video.thumbnail)
-            .setAuthor({
-              name: video.author,
-              iconURL:
-                "https://cdn.discordapp.com/avatars/890994028672319499/7750e3babbad5a777159c67668b3e649.webp",
-              url: video.channelURL,
-            })
-            .setFooter({
-              text: `Video poll posted by ${button.user.tag}`,
-              iconURL: button.user.avatarURL(),
-            });
-
-          await button.update({
-            content: "Video posted",
-            components: [createVoteButtons(true)],
-          });
-          break;
-
-        default:
-          console.warn(`Unhandled button: ${customId}`);
       }
     }
   },
