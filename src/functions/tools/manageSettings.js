e30feb578a9b97d1c59cdde0ea88756fef37db37
PrinceOfCookies <iuploadgmod@gmail.com>
2026-05-14 08:04:53 -0500

    fix: handle missing settings table during startup

--- src/functions/tools/manageSettings.js
+++ src/functions/tools/manageSettings.js
@@ -1,31 +1,48 @@
 module.exports = (client) => {
   client.getSetting = async (setting) => {
-    let dbSetting = await client.query("SELECT * FROM settings WHERE setting = ?", [
-      setting,
-    ]);
+    try {
+      let dbSetting = await client.query("SELECT * FROM settings WHERE setting = ?", [
+        setting,
+      ]);
 
-    if (dbSetting.length > 0) {
-      return Boolean(dbSetting[0].value);
-    } else {
+      if (dbSetting.length > 0) {
+        return Boolean(dbSetting[0].value);
+      } else {
+        return false;
+      }
+    } catch (error) {
+      if (error?.code === "ER_NO_SUCH_TABLE") {
+        return false;
+      }
+
+      console.error(`[${client.botName}] Failed to read setting "${setting}"`, error);
       return false;
     }
   };
 
   client.setSetting = async (setting, value) => {
-    let dbSetting = await client.query("SELECT * FROM settings WHERE setting = ?", [
-      setting,
-    ]);
+    try {
+      let dbSetting = await client.query("SELECT * FROM settings WHERE setting = ?", [
+        setting,
+      ]);
 
-    if (dbSetting.length > 0) {
-      const updatedSetting = await client.query(
-        "UPDATE settings SET value = ? WHERE setting = ?",
-        [value, setting]
-      );
-      if (updatedSetting.affectedRows > 0) {
-        return client.getSetting(setting);
+      if (dbSetting.length > 0) {
+        const updatedSetting = await client.query(
+          "UPDATE settings SET value = ? WHERE setting = ?",
+          [value, setting]
+        );
+        if (updatedSetting.affectedRows > 0) {
+          return client.getSetting(setting);
+        }
+
+        return false;
+      }
+    } catch (error) {
+      if (error?.code === "ER_NO_SUCH_TABLE") {
+        return false;
       }
 
-      return false;
+      console.error(`[${client.botName}] Failed to write setting "${setting}"`, error);
     }
   };
 };
